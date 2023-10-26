ba50f4a17a5c9bd4b57fecfae5770812b7262388
CLINT <sepalonclintairol@gmail.com>
2023-10-27 00:08:58 +0800

    Deployment Testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "./src/main.jsx",
+    },
+  },
   server: {
-    proxy:{
-      "/api/": "http://localhost:8800"
-    }
+    proxy: {
+      "/api/": "http://localhost:8800",
+    },
   },
   plugins: [react()],
-})
-
-
-
-
+});

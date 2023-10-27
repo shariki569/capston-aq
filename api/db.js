f378fde5fab1550fd6e3f00344d1d88da85456ba
CLINT <sepalonclintairol@gmail.com>
2023-10-27 16:29:38 +0800

    Adjusted the env configration

--- api/db.js
+++ api/db.js
@@ -1,13 +1,13 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./env/acc.env" });
-
+// dotenv.config({ path: "./env/acc.env" });
+dotenv.config({});
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`

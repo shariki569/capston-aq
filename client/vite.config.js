d6d59f1a1de7e023fc430b40ca6118974e865647
CLINT <sepalonclintairol@gmail.com>
2023-10-28 00:02:25 +0800

    PLEASE WORK

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dist/",
   build: {
-    manifest: true,
+    chunkSizeWarningLimit: 1500, // Adjust chunk size limit
     rollupOptions: {
-      // overwrite default .html entry
-      input: "./src/main.jsx",
-    },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
   server: {
     // proxy: {

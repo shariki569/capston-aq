870925b3f13e4570e1d06c4437d88037bdcda73e
CLINT <sepalonclintairol@gmail.com>
2023-10-28 09:10:09 +0800

    Please work PART 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
     base: "./",
     plugins: [react()],
     server: {
       proxy: {
-        "/api": process.env.VITE_APP_BACKEND_URL,
+        "/api": {
+          target: env.VITE_APP_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       },
     },
     build: {

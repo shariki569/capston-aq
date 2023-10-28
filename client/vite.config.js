0746f14ae740f4c17a32408b7d5dc1d1b2035993
CLINT <sepalonclintairol@gmail.com>
2023-10-28 08:57:30 +0800

    Fixing configurations in handling env files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,51 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  build: {
-    chunkSizeWarningLimit: 1500, // Adjust chunk size limit
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  },
-  server: {
-    // proxy: {
-    //   "/api/": "http://localhost:8800",
-    // },
-    proxy: {
-      "/api/": import.meta.env,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: "./",
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": process.env.VITE_APP_BACKEND_URL,
+      },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.split("node_modules/")[1].split("/")[0];
+            }
+          },
+        },
+      },
+    },
+  });
+};
+//  base: "./",
+//   build: {
+//     chunkSizeWarningLimit: 1500, // Adjust chunk size limit
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             return id.toString().split('node_modules/')[1].split('/')[0].toString();
+//           }
+//         }
+//       }
+//     }
+//   },
+//   server: {
+//     // proxy: {
+//     //   "/api/": "http://localhost:8800",
+//     // },
+//     proxy: {
+//       "/api/": import.meta.env.VITE_APP_BACKEND_URL,
+//     },
     
-  },
-  plugins: [react()],
-});
+//   },
+//   plugins: [react()],

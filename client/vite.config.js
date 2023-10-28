ad2a8a211d53851f45f388599d7bc5598313ee4e
CLINT <sepalonclintairol@gmail.com>
2023-10-28 11:02:25 +0800

    PLEAASE NA JUD

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
+import http from "https";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -14,6 +14,7 @@ export default ({ mode }) => {
           target: env.VITE_APP_BACKEND_URL,
           changeOrigin: true,
           secure: false,
+          agent: new http.Agent()
         }
       },
     },
